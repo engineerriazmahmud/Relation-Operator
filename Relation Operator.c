eb9ac30263fd0d9867dc41310d63684cb22183ec
engineerriazmahmud <engineerriazmahmud@gmail.com>
2022-10-05 16:27:33 +0600

    Relation Operator

---
+++ Relation Operator.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+
+{
+  int x, y, z;
+
+  x = 10;
+  y = 19;
+  z = 28;
+
+  printf("Less Than (<) operator: \n");
+  printf("\t x < y : %d\n", x < y);
+  printf("\t y < x : %d\n\n", y < x);
+
+  printf("Less Than O r Equal to (<=) operator: \n");
+  printf("\t x <= y : %d\n", x <= y);
+  printf("\t y <= x : %d\n\n", y <= x);
+
+  printf("Greater Than (>) operator: \n ");
+  printf("\t x > y %d\n", x > y);
+  printf("\t y > x %d\n\n", y > x);
+
+  printf("Greater Than Or Equal To (>=) operator: \n");
+  printf("\t x >= y %d\n", x >= y);
+  printf("\t y >= x %d\n\n", y >= x);
+
+  printf(" Equal to (==) To operator: \n");
+  printf("\t x == y: %d\n\n", y == x);
+  printf("\t y == z: %d\n\n", y == x);
+
+  printf("Not Equal To (!=) operator: \n");
+  printf("\t x != y : %d\n\n", y != x);
+  printf("\t y != z : %d\n\n", z != y);
+
+  return 0;
+}
